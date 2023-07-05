c37b684bd316ea38c795accb7b05cc442f53421d
Giacomo <giacomo.corso@gmail.com>
2023-07-05 18:25:43 +0200

    in role entity added manytomany mappedBy

--- src/main/java/dev/giacomo/jwt/models/Role.java
+++ src/main/java/dev/giacomo/jwt/models/Role.java
@@ -1,11 +1,15 @@
 package dev.giacomo.jwt.models;
 
 import lombok.Data;
+
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -20,5 +24,8 @@ public class Role {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id_role")
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
 
 }
